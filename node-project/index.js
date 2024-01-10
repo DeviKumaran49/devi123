9cc1729a08d391aa09c43aaac28101720bd261c8
leenu <153902619+leenauperumal@users.noreply.github.com>
2024-01-10 16:07:37 +0530

    leena changes

--- node-project/index.js
+++ node-project/index.js
@@ -3,3 +3,14 @@ var a=10;
 var b=20;
 var c=a+b;
 console.log(c);
+const express=require("express");
+const port=5000;
+const app=express();
+app.listen(port,()=>{
+       console.log("Welcome azure");
+})
+app.get("/api?home",(req,res)=>{
+    res.send("welcome API")
+})
+const dotenv=require("dotenv").config();
+const ports=process.env.port||5000;
